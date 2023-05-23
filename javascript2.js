214ee8725569f450335f27758b2486dbb4f95eaa
Minho Tokuhara <minho.ny@gmail.com>
2023-05-23 19:57:37 +0800

    first review

---
+++ javascript2.js
@@ -0,0 +1,10 @@
+while (true) {
+  var result = prompt("日本の首都は？");
+  if (result === "東京") {
+    alert("正解です！");
+  } else if (result === null) {
+    alert("不正解です！");
+  } else {
+    alert("不正解です！");
+  }
+}
